6d717a8d102f2e61f43eca49c8f1efabb2ae45f8
None <None>
2022-07-31 14:21:34 -0700

    partB topo WIP

---
+++ a3/partB_connect.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+# Sets bridge s0 to use OpenFlow 1.3
+ovs-vsctl set bridge s0 protocols=OpenFlow13 
+
+# Sets bridge s1 to use OpenFlow 1.3
+ovs-vsctl set bridge s1 protocols=OpenFlow13 
+
+# Sets bridge s2 to use OpenFlow 1.3
+ovs-vsctl set bridge s2 protocols=OpenFlow13
+
+# Sets bridge s3 to use OpenFlow 1.3
+ovs-vsctl set bridge s3 protocols=OpenFlow13
+
+# Sets bridge s4 to use OpenFlow 1.3
+ovs-vsctl set bridge s4 protocols=OpenFlow13 
+
+# Sets bridge s6 to use OpenFlow 1.3
+ovs-vsctl set bridge s6 protocols=OpenFlow13 
+
+# Print the protocols that each switch supports
+for switch in s0 s1 s2 s3 s4 s6;
+do
+    protos=$(ovs-vsctl get bridge $switch protocols)
+    echo "Switch $switch supports $protos"
+done
+
+# Avoid having to write "-O OpenFlow13" before all of your ovs-ofctl commands.
+ofctl='ovs-ofctl -O OpenFlow13'
+
+# OVS rules for switch 0
+# h0 -> h2
+$ofctl add-flow s0 \
+    in_port=1,ip,nw_src=10.0.0.2,nw_dst=10.0.2.2,actions=mod_dl_src:0A:00:0B:01:00:03,mod_dl_dst:0A:00:0B:FE:00:02,output=3 
+
+# h2 -> h0
+$ofctl add-flow s0 \
+    in_port=3,ip,nw_src=10.0.2.2,nw_dst=10.0.0.2,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:0A:00:00:02:00:00,output=1 
+
+
+# OVS rules for switch 1
+# h1 -> h4
+$ofctl add-flow s1 \
+    in_port=1,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:0D:01:00:03,output=3 
+
+# h4 -> h1
+$ofctl add-flow s1 \
+    in_port=3,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1 
+
+# OVS rules for switch 2
+# h0 -> h2
+$ofctl add-flow s2 \
+    in_port=2,ip,nw_src=10.0.0.2,nw_dst=10.0.2.2,actions=mod_dl_src:0A:00:02:01:00:01,mod_dl_dst:0A:00:02:02:00:00,output=1 
+
+# h1 -> h4
+$ofctl add-flow s2 \
+    in_port=3,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:FE:00:04,mod_dl_dst:0A:00:0D:FE:00:02,output=4 
+
+# h2 -> h0
+$ofctl add-flow s2 \
+    in_port=1,ip,nw_src=10.0.2.2,nw_dst=10.0.0.2,actions=mod_dl_src:0A:00:0B:FE:00:02,mod_dl_dst:0A:00:0B:01:00:03,output=2 
+
+# h4 -> h1
+$ofctl add-flow s2 \
+    in_port=4,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0D:01:00:03,mod_dl_dst:0A:00:0C:01:00:03,output=3 
+
+# OVS rules for switch 3
+# h1 -> h4
+$ofctl add-flow s3 \
+    in_port=2,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0E:01:00:03,mod_dl_dst:0A:00:0E:FE:00:02,output=3 
+
+# h3 -> h6
+$ofctl add-flow s3 \
+    in_port=1,ip,nw_src=10.0.3.2,nw_dst=10.0.6.2,actions=mod_dl_src:0A:00:0F:01:00:04,mod_dl_dst:0A:00:0F:FE:00:02,output=4 
+
+# h4 -> h1
+$ofctl add-flow s3 \
+    in_port=3,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0D:FE:00:02,mod_dl_dst:0A:00:0C:FE:00:04,output=2 
+
+# h6 -> h3
+$ofctl add-flow s3 \
+    in_port=4,ip,nw_src=10.0.6.2,nw_dst=10.0.3.2,actions=mod_dl_src:0A:00:03:01:00:01,mod_dl_dst:0A:00:03:02:00:00,output=1 
+
+# OVS rules for switch 4
+# h1 -> h4
+$ofctl add-flow s4 \
+    in_port=2,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:04:02:00:00,output=1 
+
+# h4 -> h1
+$ofctl add-flow s4 \
+    in_port=1,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:0E:01:00:03,output=2 
+
+# OVS rules for switch 6
+# h3 -> h6
+$ofctl add-flow s6 \
+    in_port=2,ip,nw_src=10.0.3.2,nw_dst=10.0.6.2,actions=mod_dl_src:0A:00:06:01:00:01,mod_dl_dst:0A:00:06:02:00:00,output=1 
+
+# h6 -> h3
+$ofctl add-flow s6 \
+    in_port=1,ip,nw_src=10.0.6.2,nw_dst=10.0.3.2,actions=mod_dl_src:0A:00:0F:FE:00:02,mod_dl_dst:0A:00:0F:01:00:04,output=2 
+
+# Print the flows installed in each switch
+for switch in s0 s1 s2 s3 s4 s6;
+do
+    echo "Flows installed in $switch:"
+    $ofctl dump-flows $switch
+    echo ""
+done
---
+++ a3/partB_topology.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+"""Topology with 10 switches and 10 hosts
+"""
+
+from mininet.cli import CLI
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.log import setLogLevel
+
+
+
+class CSLRTopo( Topo ):
+
+        def __init__( self ):
+                "Create Topology"
+
+                # Initialize topology
+                Topo.__init__( self )
+
+                # Add hosts
+                h0 = self.addHost( 'Alice' )
+                h1 = self.addHost( 'h1' )
+                h2 = self.addHost( 'h2' )
+                h3 = self.addHost( 'h3' )
+
+                # Add switches
+                s1 = self.addSwitch( 's1', listenPort=6635 )
+                s2 = self.addSwitch( 's2', listenPort=6636 )
+                s3 = self.addSwitch( 's3', listenPort=6637 )
+
+                # Add links between hosts and switches
+                self.addLink( h0, s1 ) # h0-eth0 <-> s0-eth1
+                self.addLink( h1, s2 ) # h1-eth0 <-> s1-eth1
+                self.addLink( h2, s2 ) # h2-eth0 <-> s2-eth1
+                self.addLink( h3, s3 ) # h3-eth0 <-> s3-eth1
+
+                # Add links between switches, with bandwidth 100Mbps
+
+
+def run():
+        "Create and configure network"
+        topo = CSLRTopo()
+        net = Mininet( topo=topo, link=TCLink, controller=None )
+
+        # Set interface IP and MAC addresses for hosts
+        h0 = net.get( 'Alice' )
+        h0.intf( 'Alice-eth0' ).setIP( '10.1.1.17', 24 )
+        h0.intf( 'Alice-eth0' ).setMAC( 'AA:AA:AA:AA:AA:AA' )
+
+        h1 = net.get( 'h1' )
+        h1.intf( 'h1-eth0' ).setIP( '10.0.1.2', 24 )
+        h1.intf( 'h1-eth0' ).setMAC( '0A:00:01:02:00:00' )
+
+        h2 = net.get( 'h2' )
+        h2.intf( 'h2-eth0' ).setIP( '10.0.2.2', 24 )
+        h2.intf( 'h2-eth0' ).setMAC( '0A:00:02:02:00:00' )
+
+        h3 = net.get( 'h3' )
+        h3.intf( 'h3-eth0' ).setIP( '10.0.3.2', 24 )
+        h3.intf( 'h3-eth0' ).setMAC( '0A:00:03:02:00:00' )
+
+        # Set interface MAC address for switches (NOTE: IP
+        # addresses are not assigned to switch interfaces)
+        s1 = net.get( 's1' )
+        s1.intf( 's1-eth1' ).setMAC( '0A:00:01:01:00:01' )
+        #s1.intf( 's1-eth2' ).setMAC( '0A:00:0A:FE:00:02' )
+
+        s2 = net.get( 's2' )
+        s2.intf( 's2-eth1' ).setMAC( '0A:00:02:01:00:01' )
+        s2.intf( 's2-eth2' ).setMAC( '0A:00:0B:FE:00:02' )
+        #s2.intf( 's2-eth3' ).setMAC( '0A:00:0D:01:00:03' )
+        #s2.intf( 's2-eth4' ).setMAC( '0A:00:0C:FE:00:04' )
+
+        s3 = net.get( 's3' )
+        s3.intf( 's3-eth1' ).setMAC( '0A:00:03:01:00:01' )
+        #s3.intf( 's3-eth2' ).setMAC( '0A:00:0D:FE:00:02' )
+
+
+
+        net.start()
+
+        # Add routing table entries for hosts (NOTE: The gateway
+		# IPs 10.0.X.1 are not assigned to switch interfaces)
+        h0.cmd( 'route add default gw 10.1.1.14 dev Alice-eth0' )
+        h1.cmd( 'route add default gw 10.0.1.1 dev h1-eth0' )
+        h2.cmd( 'route add default gw 10.0.2.1 dev h2-eth0' )
+        h3.cmd( 'route add default gw 10.0.3.1 dev h3-eth0' )
+
+
+        # Add arp cache entries for hosts
+        h0.cmd( 'arp -s 10.1.1.14 0A:00:00:01:00:01 -i Alice-eth0' )
+        h1.cmd( 'arp -s 10.0.1.1 0A:00:01:01:00:01 -i h1-eth0' )
+        h2.cmd( 'arp -s 10.0.2.1 0A:00:02:01:00:01 -i h2-eth0' )
+        h3.cmd( 'arp -s 10.0.3.1 0A:00:03:01:00:01 -i h3-eth0' )
+
+
+        # Open Mininet Command Line Interface
+        CLI(net)
+
+        # Teardown and cleanup
+        net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
